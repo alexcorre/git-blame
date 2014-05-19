b18c29c828c7ad8121fe7ce56d9798dc8f6f2f93
Alex Corre <aecorre@linkedin.com>
2014-05-19 11:10:24 -0700

    add unit tests for remote revision. remove sample unit test files

---
+++ spec/RemoteRevision-spec.js
@@ -0,0 +1,119 @@
+var RemoteRevision = require('../lib/util/RemoteRevision');
+
+describe('RemoteRevision', function() {
+
+  var DEFAULT_HASH = '12345';
+  var instance;
+
+  beforeEach(function () {
+    instance = new RemoteRevision();
+  });
+
+  describe('verifyTemplateData', function () {
+
+    it('Should return true if project, repo, and revision are defined', function () {
+      var data = {
+        project: 'some proj',
+        repo: 'some repo',
+        revision: '12345'
+      };
+
+      var isValidData = instance.verifyTemplateData(data);
+      expect(isValidData).toBe(true);
+    });
+
+    it('Should return false if only revision is defined', function () {
+      var data = {
+        revision: '12345'
+      };
+
+      var isValidData = instance.verifyTemplateData(data);
+      expect(isValidData).toBe(false);
+    });
+
+  });
+
+  describe('parseProjectAndRepo', function() {
+
+    beforeEach(function () {
+      instance.hash = DEFAULT_HASH;
+      instance.remote = null;
+    });
+
+    afterEach(function () {
+      instance.hash = null;
+      instance.remote = null;
+    });
+
+    it('Should return an empty object if pattern does not match', function () {
+      var weirdRemote = 'NOT_MATCHING';
+      instance.remote = weirdRemote;
+
+      var output = instance.parseProjectAndRepo();
+      expect(output).toEqual({});
+    });
+
+
+    it('Should parse a standard github url correctly', function () {
+      var githubRemote = 'git@github.com:project/someRepo.git';
+      instance.remote = githubRemote;
+
+      var output = instance.parseProjectAndRepo();
+
+      expect(output).toEqual({
+        project: 'project',
+        repo: 'someRepo'
+      });
+    });
+
+    it('Should parse a read only github url correctly', function () {
+      var githubHttpRemote = 'https://github.com/project/someRepo.git';
+      instance.remote = githubHttpRemote;
+
+      var output = instance.parseProjectAndRepo();
+
+      expect(output).toEqual({
+        project: 'project',
+        repo: 'someRepo'
+      });
+    });
+
+    it('Should parse a repo url with dashes', function () {
+      var githubHttpRemote = 'https://github.com/some-project/some-repo.git';
+      instance.remote = githubHttpRemote;
+
+      var output = instance.parseProjectAndRepo();
+
+      expect(output).toEqual({
+        project: 'some-project',
+        repo: 'some-repo'
+      });
+    });
+
+    it('Should work with a url with a port', function() {
+      var portRemoteUrl = 'ssh://git@git.my-company.com:2222/group/repo-name.git';
+      instance.remote = portRemoteUrl;
+
+      var output = instance.parseProjectAndRepo();
+
+      expect(output).toEqual({
+        project: 'group',
+        repo: 'repo-name'
+      });
+    });
+
+    it('Should work with a url with a port and colon', function() {
+      var portRemoteUrl = 'git@git.my-company.com:2222:group/repo-name.git';
+      instance.remote = portRemoteUrl;
+
+      var output = instance.parseProjectAndRepo();
+
+      expect(output).toEqual({
+        project: 'group',
+        repo: 'repo-name'
+      });
+    });
+
+  });
+
+});
--- spec/git-blame-view-spec.coffee
+++
@@ -1,6 +0,0 @@
-GitBlameView = require '../lib/git-blame-view'
-{WorkspaceView} = require 'atom'
-
-describe "GitBlameView", ->
-  it "has one valid test", ->
-    expect("life").toBe "easy"
